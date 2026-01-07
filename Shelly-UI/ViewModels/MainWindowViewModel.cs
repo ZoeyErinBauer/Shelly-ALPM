6ef99abaf8751901a5ef4cdf1c6506d00c43a106
Zoey <zoey.erin.bauer@gmail.com>
2026-01-07 16:47:33 -0500

    Integrate Material Icons and SplitView for navigation layout, add debugging support in DEBUG mode

--- Shelly-UI/ViewModels/MainWindowViewModel.cs
+++ Shelly-UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace Shelly_UI.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+using Material.Icons;
+
+namespace Shelly_UI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Greeting { get; } = "Welcome to Shelly!";
+
+    private bool _isPaneOpen = true;
+    public bool IsPaneOpen
+    {
+        get => _isPaneOpen;
+        set => this.RaiseAndSetIfChanged(ref _isPaneOpen, value);
+    }
+
+    public ObservableCollection<MenuItemViewModel> MenuItems { get; } = new()
+    {
+        new MenuItemViewModel("Home", MaterialIconKind.Home),
+        new MenuItemViewModel("Packages", MaterialIconKind.PackageVariantClosed),
+        new MenuItemViewModel("Updates", MaterialIconKind.Update),
+        new MenuItemViewModel("Settings", MaterialIconKind.Settings)
+    };
+
+    public void TogglePane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+}
+
+public class MenuItemViewModel(string label, MaterialIconKind icon)
+{
+    public string Label { get; } = label;
+    public MaterialIconKind Icon { get; } = icon;
 }
