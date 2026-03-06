90f4b80ac40d82f8172e193bf06b8f2c1dc27a85
caroline <carolinemariesnyder@gmail.com>
2026-03-05 23:18:49 -0500

    Pushing Aur GObject with package

---
+++ Shelly.Gtk/UiModels/AUR/GObjects/AurPackageGObject.cs
@@ -0,0 +1,19 @@
+using GObject;
+using Shelly.Gtk.UiModels.PackageManagerObjects;
+
+namespace Shelly.Gtk.UiModels.AUR.GObjects;
+
+[Subclass<GObject.Object>]
+public partial class AurPackageGObject
+{
+    public AurPackageDto? Package { get; set; }
+    public bool IsSelected { get; set; }
+
+    public event EventHandler? OnSelectionToggled;
+
+    public void ToggleSelection()
+    {
+        IsSelected = !IsSelected;
+        OnSelectionToggled?.Invoke(this, EventArgs.Empty);
+    }
+}
