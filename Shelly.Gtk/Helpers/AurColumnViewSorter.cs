b89c8397c01df653e5b760ff80720321ea424fb5
VinnyQF <viniqf@gmail.com>
2026-05-01 15:07:58 -0300

    Replicating sorting to the AUR updates window.

--- Shelly.Gtk/Helpers/AurColumnViewSorter.cs
+++ Shelly.Gtk/Helpers/AurColumnViewSorter.cs
@@ -39,15 +39,49 @@ public static class AurColumnViewSorter
         );
     }
 
+    // Adding this for AUR updates
+    public static void Sort(
+        Gio.ListStore listStore,
+        List<AurUpdateGObject> items,
+        PackageSortColumn column,
+        SortType order)
+    {
+        Comparison<AurUpdateGObject> comparison =
+            column switch
+            {
+                PackageSortColumn.Name =>
+                    (a, b) => Compare(
+                        a.Package?.Name,
+                        b.Package?.Name
+                    ),
+
+                PackageSortColumn.Version =>
+                    (a, b) => Compare(
+                        a.Package?.Version,
+                        b.Package?.Version
+                    ),
+
+                _ => (_, _) => 0
+            };
+
+        SortInternal(
+            listStore,
+            items,
+            comparison,
+            order
+        );
+    }
+
     /*
      * Shared implementation
      */
 
-    private static void SortInternal(
+    private static void SortInternal<T>(
         Gio.ListStore listStore,
-        List<AurPackageGObject> items,
-        Comparison<AurPackageGObject> comparison,
+        List<T> items,
+        Comparison<T> comparison,
         SortType order)
+        where T : GObject.Object
     {
         if (order == SortType.Descending)
         {
@@ -76,9 +110,10 @@ public static class AurColumnViewSorter
         );
     }
 
-    private static void SpliceReplace(
+    private static void SpliceReplace<T>(
         Gio.ListStore listStore,
-        List<AurPackageGObject> items)
+        List<T> items)
+        where T : GObject.Object
     {
         var array = new GObject.Object[items.Count];
 
